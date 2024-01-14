41d6090f9721d7b99b63769a70650af0126ac6a1
meghla khatun <meghlaparvin45@gmail.com>
2024-01-14 22:43:53 +0600

    added a owl carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,29 @@
+// header section
+
 const menuBar = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu');
 menuBar.addEventListener('click', ()=>{
 	menu.classList.toggle("active")
 })
 
-
-
+// banner section
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 		
 
 
